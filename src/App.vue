0bdff0e907d980ee4d46c0a16f71069c67a2f583
Henry Rincón Santana <henry.rincon@globalmvm.com>
2026-05-22 10:13:21 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-layout>
+    <v-navigation-drawer>
+      <v-list nav>
+        <v-list-item title="Navigation drawer" link></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar title="Application bar"></v-app-bar>
+
+    <v-main class="bg-primary" height="100vh">
+      <v-container>
+        <v-sheet
+          border="dashed md"
+          color="surface-light"
+          height="200"
+          rounded="lg"
+          width="100%"
+        ></v-sheet>
+      </v-container>
+    </v-main>
+  </v-layout>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import '@fontsource/roboto/100.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/roboto/900.css';
+import '@mdi/font/css/materialdesignicons.css';
+import { createPinia } from 'pinia';
+import { createApp } from 'vue';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import 'vuetify/styles';
+import App from './App.vue';
+import './assets/main.css';
+import router from './router';
+const app = createApp(App);
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+app.use(createPinia());
+app.use(router);
+app.use(vuetify);
+app.mount('#app');
